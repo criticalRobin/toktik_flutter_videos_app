885ab75a4ee5f7a5692aa1c22b21378c49627c58
matite <matiastite1@gmail.com>
2024-02-02 10:19:44 -0500

    Implemented function loadNextPage to read and asign the videos to the list of videos of our provider

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/infrastructure/models/local_video_model.dart';
+import 'package:toktik/shared/data/local_video_posts.dart';
 
 class DiscoverProvider extends ChangeNotifier {
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
   Future<void> loadNextPage() async {
-    // To Do: load videos
+    // ToDo: load videos
+    await Future.delayed(const Duration(seconds: 2));
+    final List<VideoPost> newVideos = videoPosts
+        .map((video) => LocalVideoModel.fromJsonMap(video).toVideoPostEntity())
+        .toList();
 
+    videos.addAll(newVideos);
+    initialLoading = false;
     notifyListeners();
   }
 }
