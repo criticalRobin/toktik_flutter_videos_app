c1ed46fc1c738916664ee0d5f43d88b65b0c9208
matite <matiastite1@gmail.com>
2024-02-02 10:41:51 -0500

    Created widget to show the videos on screen

---
+++ lib/widgets/shared/video_scrollable_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+
+class VideoScrollableView extends StatelessWidget {
+  final List<VideoPost> videos;
+
+  const VideoScrollableView({super.key, required this.videos});
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      scrollDirection: Axis.vertical,
+      children: [
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.blue,
+        ),
+      ],
+    );
+  }
+}
