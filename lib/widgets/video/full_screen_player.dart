67765690b66905b2a63472f50a76906840ed46ed
matite <matiastite1@gmail.com>
2024-02-05 10:50:45 -0500

    Implemented pause and play action and backgound gradient

--- lib/widgets/video/full_screen_player.dart
+++ lib/widgets/video/full_screen_player.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toktik/widgets/video/video_background.dart';
 import 'package:video_player/video_player.dart';
 
 class FullScreenPlayer extends StatefulWidget {
@@ -37,12 +38,58 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
       future: controller.initialize(),
       // snapshot es el estado del future
       builder: (context, snapshot) {
-        return const Center(
-          child: CircularProgressIndicator(
-            strokeWidth: 2,
+        if (snapshot.connectionState != ConnectionState.done) {
+          return const Center(
+            child: CircularProgressIndicator(
+              strokeWidth: 2,
+            ),
+          );
+        }
+        return GestureDetector(
+          onTap: () {
+            if (controller.value.isPlaying) {
+              controller.pause();
+              return;
+            }
+            controller.play();
+          },
+          child: AspectRatio(
+            aspectRatio: controller.value.aspectRatio,
+            child: Stack(
+              children: [
+                VideoPlayer(controller),
+                VideoBackground(stops: const [0.8, 1.0],),
+                Positioned(
+                  bottom: 50,
+                  left: 20,
+                  child: _VideoCaption(caption: widget.caption),
+                )
+              ],
+            ),
           ),
         );
       },
+    );
+  }
+}
+
+class _VideoCaption extends StatelessWidget {
+  final String caption;
+
+  const _VideoCaption({required this.caption});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+
+    return SizedBox(
+      width: size.width * 0.6,
+      child: Text(
+        caption,
+        maxLines: 2,
+        style: titleStyle,
+      ),
     );
   }
 }
